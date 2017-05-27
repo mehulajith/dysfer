f10acf12646b21f2543d8859daabbcd4653304c5
mehulajith <Mehul@MacBook-Pro-5.local>
2017-05-27 13:40:05 -0500

    main project

---
+++ dsyfer/dsyfer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dsyfer
+//
+//  Created by Mehul Ajith on 5/27/17.
+//  Copyright © 2017 Mehul Ajith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
