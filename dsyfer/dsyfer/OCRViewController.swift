a96794d2a15d8075fbaa99a538e836641a26be6b
mehulajith <Mehul@MacBook-Pro-5.local>
2017-05-27 20:53:22 -0500

    removed old code

--- dsyfer/dsyfer/OCRViewController.swift
+++ dsyfer/dsyfer/OCRViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import TessereactOCR
+import TesseractOCR
 
 class OCRViewController: UIViewController {
     
@@ -47,20 +47,4 @@ class OCRViewController: UIViewController {
     }
     */
 
-}
-
-extension UIImage {
-    func fixOrientation() -> UIImage {
-        if self.imageOrientation == UIImageOrientation.up {
-            return self
-        }
-        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
-        self.draw(in: CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height))
-        if let normalizedImage: UIImage = UIGraphicsGetImageFromCurrentImageContext() {
-            UIGraphicsEndImageContext()
-            return normalizedImage
-        } else {
-            return self
-        }
-    }
 }
